1c5b3d36334d567fd11e567be0ce51ae3bc8dc2d
gaetan <170516@supinfo.com>
2016-02-29 15:31:36 +0100

    oauth2 zendesk + modif hask/clientId

--- src/Stadline/PublicConnectorBundle/Controller/DefaultController.php
+++ src/Stadline/PublicConnectorBundle/Controller/DefaultController.php
@@ -6,10 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function widgetAction($clientId)
+    public function widgetAction($hash)
     {
-        $zendeskClient = $this->get('stadline_zendesk_client');
+//        $zendeskClient = $this->get('stadline_zendesk_client');
 
-        return $this->render('StadlinePublicConnectorBundle:Default:index.html.twig', array('client_id' => $clientId));
+        /* Zendesk get auth */
+//        $auth = $zendeskClient->getAuthorization(array("response_type" => "code", "redirect_uri" => "http://lundimatin.local/app_dev.php/widget/1/links", "client_id" => $clientId, "scope" => "read write"))->getObjectResponse();
+//        var_dump($auth);
+//        die();
+
+//        $auth = $zendeskClient->getAuth();
+//        var_dump($auth);
+//        die();
+
+        $em = $this->get('doctrine.orm.entity_manager');
+        $contact = $em
+            ->getRepository("StadlineFrontBundle:Contact")
+            ->findOneByHashedRef($hash)
+        ;
+
+        $clientLmbId = null;
+        if($contact != null){
+            $clientLmbId = $contact->getRef();
+        }
+
+        return $this->render('StadlinePublicConnectorBundle:Default:index.html.twig', array(
+            'client_lmb_id' => $clientLmbId,
+            'hash_ref' => $hash
+        ));
     }
 }
