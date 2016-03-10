a59170ab0bfeeb8b16fd5b7fe55e5336a96aed1d
gaetan <170516@supinfo.com>
2016-03-10 11:42:52 +0100

    modif design show / edit

--- src/Stadline/PublicConnectorBundle/Controller/DefaultController.php
+++ src/Stadline/PublicConnectorBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace Stadline\PublicConnectorBundle\Controller;
 
+use Guzzle\Http\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -18,11 +19,6 @@ class DefaultController extends Controller
 //        $auth = $zendeskClient->getAuth();
 //        var_dump($auth);
 //        die();
-//
-//        $test = curl_init("https://extraclub.zendesk.com/api/v2/tickets.json -u amandine.fournier@stadline.com/token:yhxWNYL0XQW8Vo5JQOaZBpdJJMkbcwcNdxN5yYmQ");
-//        $exec = curl_exec($test);
-//        var_dump($exec);
-//        die();
 
         $em = $this->get('doctrine.orm.entity_manager');
 
@@ -35,6 +31,24 @@ class DefaultController extends Controller
         if($contact != null){
             $clientLmbId = $contact->getRef();
         }
+
+
+        /* Test Guzzle */
+        $client = new Client();
+        $request = $client->get('https://extraclub.zendesk.com/oauth/authorizations/new');
+        $query = $request->getQuery();
+        $query->add("client_id","extraclub");
+        $query->add("response_type","code");
+        $query->add("redirect_uri","https://extraclub.zendesk.com/hc/fr/requests");
+        $query->add("scope","read write");
+
+        $client2 = new Client();
+        $request2 = $client2->get('https://extraclub.zendesk.com/api/v2/tickets.json', array("Authorization" => "Bearer ac4432ba2f8bfca69f956ba988a3d03b69696cb44d04e963e73c9cc7de8db91a"), array());
+
+        $response = $request2->send();
+
+//        var_dump($response);
+//        die();
 
         return $this->render('StadlinePublicConnectorBundle:Default:index.html.twig', array(
             'client_lmb_id' => $clientLmbId,
